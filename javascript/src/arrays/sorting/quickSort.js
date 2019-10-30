086dddbab4fd5d9ff9f844ac34a209d7c2d38008
Igor Balos <ibalosh@gmail.com>
2019-10-30 10:52:00 +0100

    added quick sort

---
+++ javascript/src/arrays/sorting/quickSort.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * @param {number[]} arr
+ * @returns {number[]}
+ */
+function quickSort(arr, left = 0, right = arr.length-1) {
+    let pivot = partition(arr, left, right);
+
+    if (left < pivot -1)
+      quickSort(arr, left, pivot -1);
+
+    if (right > pivot )
+      quickSort(arr, pivot, right);
+}
+
+function partition(arr, left, right) {
+  let pivot = Math.floor((left + right) / 2);
+
+  while (left <= right) {
+    while (arr[left] < arr[pivot])
+      left++;
+
+    while (arr[right] > arr[pivot])
+      right--;
+
+    if (left <= right) {
+      let temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+  return left;
+}
+
+let arr= [4,3,6,1,5];
+quickSort(arr);
+console.log(arr);
+
