0f532f61941dd857850a1eb3f0efc82ff5a1a674
Igor Balos <ibalosh@gmail.com>
2019-11-22 08:56:14 +0100

    added new algorithms

--- src/arrays/twoPointers/mozeZeroes.js
+++ src/arrays/twoPointers/mozeZeroes.js
@@ -1,3 +1,35 @@
+/**
+ * 283. Move Zeroes
+ * https://leetcode.com/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75
+ * -------------------------------------------------------------
+ *
+ * Given an integer array nums, move all 0's to the end of it while maintaining
+ * the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Example 2:
+ *
+ * Input: nums = [0]
+ * Output: [0]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
+ * -------------------------------------------------------------
+ *
+ * Runtime complexity: O(n)
+ * Space complexity: O(1)
+ *
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
 let moveZeroes = function(nums) {
     let indexOfFreeSpace = 0;
 
@@ -11,15 +43,16 @@ let moveZeroes = function(nums) {
     for (let i = indexOfFreeSpace; i < nums.length;i++) {
       nums[i] = 0;
     }
-
-    return nums;
 };
 
 let nums = [0,1,0,3,12];
-console.log(moveZeroes(nums));
+moveZeroes(nums)
+console.log(nums);
 
 nums = [0];
-console.log(moveZeroes(nums));
+moveZeroes(nums)
+console.log(nums);
 
 nums = [0,1];
-console.log(moveZeroes(nums));
+moveZeroes(nums)
+console.log(nums);
