e74269e9a4ec954fb1b32940305174ec8919f38d
Igor Balos <ibalosh@gmail.com>
2020-11-28 11:25:16 +0100

    added three sum problems to the list

---
+++ src/arrays/twoPointers/threeSumHash.js
@@ -0,0 +1,82 @@
+/**
+ * 15. 3Sum
+ * https://leetcode.com/problems/3sum/description/?envType=study-plan-v2&envId=top-interview-150
+ * -----------------------------------------------------------------------
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+ * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Explanation:
+ * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+ * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+ * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+ * The distinct triplets are [-1,0,1] and [-1,-1,2].
+ * Notice that the order of the output and the order of the triplets does not matter.
+ *
+ * Example 2:
+ *
+ * Input: nums = [0,1,1]
+ * Output: []
+ * Explanation: The only possible triplet does not sum up to 0.
+ * Example 3:
+ *
+ * Input: nums = [0,0,0]
+ * Output: [[0,0,0]]
+ * Explanation: The only possible triplet sums up to 0.
+ *
+ * Constraints:
+ *
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ * -----------------------------------------------------------------------
+ * Runtime complexity: O(n^2) + O(n*logn) ~= O(n^2)
+ * Space complexity: O(n)
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let threeSum = function(nums, target) {
+  function twoSum(nums, i, target, values) {
+    let seen = new Set();
+
+    for (let j=i+1;j<nums.length;j++) {
+      const complement = target + (-nums[i] - nums[j]);
+
+      if (seen.has(complement)) {
+        values.push([nums[i], nums[j], complement]);
+        while (nums[j] === nums[j+1]) j++;
+      }
+
+      seen.add(nums[j]);
+    }
+  }
+
+  nums.sort((a,b) => a - b);
+  let values = [];
+
+  for (let i=0;i<nums.length;i++) {
+    if (nums[i] > 0) continue;
+    if (nums[i-1] === nums[i]) continue;
+    twoSum(nums, i, target, values)
+  }
+
+  return values;
+};
+
+// -4, -1, -1, 0, 1, 2
+let nums = [-1,0,1,2,-1,-4];
+console.log(threeSum(nums, 0)); // [[-1,-1,2],[-1,0,1]]
+
+nums = [0,1,1]
+console.log(threeSum(nums, 0)); // []
+
+nums = [0,0,0,0]
+console.log(threeSum(nums, 0)); // []
+
+nums = [-2,0,0,2,2]
+console.log(threeSum(nums, 0)); // []
