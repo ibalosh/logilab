a9b7ab083a0d1b4a4c438c54987a041841b07c08
Igor Balos <ibalosh@gmail.com>
2020-12-30 12:57:42 +0100

    fix quicksort

--- src/arrays/sorting/quickSort.js
+++ src/arrays/sorting/quickSort.js
@@ -1,27 +1,25 @@
 /**
  *
  * @param {number[]} arr
- * @returns {number[]}
+ * @param {number}
+ * @param {number}
  */
-function quickSort(arr, left = 0, right = arr.length-1) {
-    let pivot = partition(arr, left, right);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = partition(arr, left, right);
 
-    if (left < pivot -1)
-      quickSort(arr, left, pivot -1);
-
-    if (right > pivot )
-      quickSort(arr, pivot, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex, right);
+  }
 }
 
 function partition(arr, left, right) {
-  let pivot = Math.floor((left + right) / 2);
+  let pivot = arr[Math.floor((left + right) / 2)];
 
   while (left <= right) {
-    while (arr[left] < arr[pivot])
-      left++;
+    while (arr[left] < pivot) left++;
 
-    while (arr[right] > arr[pivot])
-      right--;
+    while (arr[right] > pivot) right--;
 
     if (left <= right) {
       let temp = arr[left];
@@ -32,6 +30,7 @@ function partition(arr, left, right) {
     }
   }
 
+  // Return the partition index
   return left;
 }
 
