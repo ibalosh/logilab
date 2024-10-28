c3204cf04a77e62e5419f6a41a04c3855127ac5f
Igor Balos <ibalosh@gmail.com>
2024-10-28 10:53:36 +0100

    add binary search examples

--- javascript/src/binarySearch/guessNumberHigherLower.js
+++ javascript/src/binarySearch/guessNumberHigherLower.js
@@ -1,16 +1,45 @@
 /**
  * Forward declaration of guess API.
  * @param {number} num   your guess
+ * @param {number} pick   your guess
  * @return 	     -1 if num is higher than the picked number
  *			      1 if num is lower than the picked number
  *               otherwise return 0
- * var guess = function(num) {}
- */
+**/
+
+const guess = function(num, pick = 2) {
+  if (num > pick) return -1;
+  else if (num < pick) return 1;
+  else return 0;
+
+}
 
 /**
  * @param {number} n
  * @return {number}
  */
-var guessNumber = function(n) {
+let guessNumber = function(n) {
+  let left = 1;
+  let right = n;
 
+  while (left <= right) {
+    let mid = Math.floor(left + (right-left)/2);
+    let result = guess(mid);
+
+    if (result === -1) {
+      right = mid - 1;
+    }
+    else if (result === 1) {
+      left = mid + 1;
+    }
+    else {
+      return mid;
+    }
+  }
 };
+
+let n = 2;
+let pick = 6;
+
+console.log(guessNumber(n));
+
