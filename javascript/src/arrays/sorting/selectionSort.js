faa14fbfa017587b30a5e07b483ba7f386ac024c
Igor Balos <ibalosh@gmail.com>
2024-10-30 09:31:57 +0100

    fix algo

--- javascript/src/arrays/sorting/selectionSort.js
+++ javascript/src/arrays/sorting/selectionSort.js
@@ -6,7 +6,7 @@ function selectionSort(arr) {
   }
 
   for (let i = 0; i < arr.length-1; i++) {
-    for (let j = i+1; j < arr.length - 1; j++) {
+    for (let j = i+1; j < arr.length; j++) {
       if (arr[j] < arr[i]) {
         swap(arr, i, j);
       }
