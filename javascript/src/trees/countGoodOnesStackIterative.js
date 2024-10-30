cfd58558d5aa9c3ede0ab7390e8ec03cdf37096d
Igor Balos <ibalosh@gmail.com>
2024-10-30 12:41:58 +0100

    add bfs,dfs, recursive dfs solutions

---
+++ javascript/src/trees/countGoodOnesStackIterative.js
@@ -0,0 +1,83 @@
+import {TreeNode} from "./TreeNode.js";
+
+/**
+ * Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+ *
+ * Return the number of good nodes in the binary tree.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ *
+ * Input: root = [3,1,4,3,null,1,5]
+ * Output: 4
+ * Explanation: Nodes in blue are good.
+ * Root Node (3) is always a good node.
+ * Node 4 -> (3,4) is the maximum value in the path starting from the root.
+ * Node 5 -> (3,4,5) is the maximum value in the path
+ * Node 3 -> (3,1,3) is the maximum value in the path.
+ * Example 2:
+ *
+ *
+ *
+ * Input: root = [3,3,null,4,2]
+ * Output: 3
+ * Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+ * Example 3:
+ *
+ * Input: root = [1]
+ * Output: 1
+ * Explanation: Root is considered as good.
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the binary tree is in the range [1, 10^5].
+ * Each node's value is between [-10^4, 10^4].
+ *
+ *
+ * Runtime: O(n)
+ * Space: O(n) - since you are putting on stack max, and in worst case scenario,
+ * if height = n (when tree has only one path, node left for example
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+let goodNodesIterative = function(root) {
+  let count = 0
+  let stackPair = [];
+  stackPair.push({ max: root.val, node: root});
+
+  while (stackPair.length > 0) {
+    let {max, node} = stackPair.pop();
+    if (node.val >= max) {
+      count++;
+      max = node.val;
+    }
+
+    if (node.left !== null)
+      stackPair.push({ max, node: node.left });
+
+    if (node.right !== null)
+      stackPair.push({ max, node: node.right });
+  }
+
+  return count;
+};
+
+
+let tree = new TreeNode(
+  3,
+  new TreeNode(1,
+    new TreeNode(3),
+    new TreeNode(10)
+  ),
+  new TreeNode(4,
+    new TreeNode(1),
+    new TreeNode(5),
+  )
+);
+
+console.log(goodNodesIterative(tree));
