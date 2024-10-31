8753265eb490412c68e213bb406b871763ede13b
Igor Balos <ibalosh@gmail.com>
2024-10-31 11:48:42 +0100

    add new tree solutions

---
+++ javascript/src/trees/lowestCommonAncestor.js
@@ -0,0 +1,44 @@
+import {TreeNode} from "./TreeNode.js";
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+let lowestCommonAncestor = function(root, p, q) {
+  let result = null;
+   function dfs(node, p, q) {
+     if (node === null) return 0;
+     let left = dfs(node.left, p, q) ? 1 : 0;
+     let right = dfs(node.right, p, q) ? 1 : 0;
+
+     let mid = (node.val === p || node.val === q) ? 1 : 0
+     if (left + right + mid >=2) {
+       result = node;
+     }
+
+     return (left + right + mid > 0)
+   }
+
+   dfs(root, p.val, q.val);
+
+   return result;
+};
+
+let tree = new TreeNode(
+  3,
+  new TreeNode(5,
+    new TreeNode(6),
+    new TreeNode(2,
+      new TreeNode(7),
+      new TreeNode(4)
+    )
+  ),
+  new TreeNode(1,
+    new TreeNode(0),
+    new TreeNode(8)
+  )
+);
+
+console.log(lowestCommonAncestor(tree, new TreeNode(5), new TreeNode(1)))
